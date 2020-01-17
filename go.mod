265919edfb9bf4f63a9b56c9eb1823163aa9db7e
Paweł Sołtysiak <pawel@soltysiak.it>
2020-01-17 15:26:41 +0100

    code init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/soltys/go-aftermarketpl
+
+go 1.13
---
+++ transport.go
@@ -0,0 +1,63 @@
+package aftermarketpl
+
+import (
+	"http"
+	"io/ioutil"
+	"json"
+	"log"
+)
+
+type Aftermarketpl struct {
+	url    string
+	key    string
+	secret string
+}
+
+func New(key, secret string) *Aftermarketpl {
+	return &Aftermarketpl{
+		key:    key,
+		secret: secret,
+		url:    "https://json.aftermarket.pl",
+	}
+}
+
+func NewCustomUrl(key, secret, url string) *Aftermarketpl {
+	return &Aftermarketpl{
+		key:    key,
+		secret: secret,
+		url:    url,
+	}
+}
+
+func (a *Aftermarketpl) Send(command string, params interface{}) ([]byte, error) {
+	requestURL := a.url + command
+	
+	requestBody, err := json.Marshal(params)
+
+	if err != nil {
+		return nil, err
+	}
+
+	request, err := http.NewRequest("POST", requestURL, requestBody)
+
+	if err != nil {
+		return nil, err
+	}
+
+	client := &http.Client{}
+
+	response, err := client.Do(request)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
