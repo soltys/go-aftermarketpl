4b7e724f37fbb4446996dd119e3c795145d39e95
Paweł Sołtysiak <pawel@soltysiak.it>
2020-01-17 21:06:24 +0100

    added logging some response

--- domainDns.go
+++ domainDns.go
@@ -1,13 +1,13 @@
 package aftermarketpl
-
+import "log"
 type domainDNSListRequest struct {
 	name string `url:"name"`
 }
 
 // DomainDNSList returns the list of domain DNS entries.
 func (a *Aftermarketpl) DomainDNSList(name string) error {
-	_, err := a.Send("/domain/dns/list", domainDNSListRequest{name: name})
-
+	response, err := a.Send("/domain/dns/list", domainDNSListRequest{name: name})
+	log.Println(string(response))
 	if err != nil {
 		return err
 	}
