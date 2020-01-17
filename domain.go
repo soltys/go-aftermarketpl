86511d97dc17fed62bfb292449d407ad1c7b6f29
Paweł Sołtysiak <pawel@soltysiak.it>
2020-01-17 21:39:18 +0100

    added logging

--- domain.go
+++ domain.go
@@ -1,16 +1,20 @@
 package aftermarketpl
 
+import "log"
+
 type domainGetRequest struct {
 	Name string `url:"name"`
 }
 
-//DomainGet  returns the domain information on user account.
+// DomainGet  returns the domain information on user account.
 func (a *Aftermarketpl) DomainGet(name string) error {
-	_, err := a.Send("/domain/get", domainGetRequest{Name: name})
+	response, err := a.Send("/domain/get", domainGetRequest{Name: name})
 
 	if err != nil {
 		return err
 	}
+
+	log.Printf("%s", response)
 
 	return nil
 }
--- domainDns.go
+++ domainDns.go
@@ -1,5 +1,7 @@
 package aftermarketpl
 
+import "log"
+
 type domainDNSListRequest struct {
 	Name string `url:"name"`
 }
@@ -7,7 +9,7 @@ type domainDNSListRequest struct {
 // DomainDNSList returns the list of domain DNS entries.
 func (a *Aftermarketpl) DomainDNSList(name string) error {
 	response, err := a.Send("/domain/dns/list", domainDNSListRequest{Name: name})
-	
+	log.Println(string(response))
 	if err != nil {
 		return err
 	}
