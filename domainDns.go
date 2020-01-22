3ac0b9b2431ad94b48c70beedcdf6eb10d833827
Paweł Sołtysiak <pawel@soltysiak.it>
2020-01-22 17:49:09 +0100

    reset of dns domain methods

--- domainDns.go
+++ domainDns.go
@@ -1,6 +1,6 @@
 package aftermarketpl
 
-type domainDNSListRequest struct {
+type domainNameRequest struct {
 	Name string `url:"name"`
 }
 
@@ -19,6 +19,63 @@ type DomainDNSListEntry struct {
 // DomainDNSList returns the list of domain DNS entries.
 func (a *Aftermarketpl) DomainDNSList(name string) ([]DomainDNSListEntry, error) {
 	d := domainDNSListResponse{}
-	err := a.Do("/domain/dns/list", domainDNSListRequest{Name: name}, &d)
+	err := a.Do("/domain/dns/list", domainNameRequest{Name: name}, &d)
+	return d.Data, err
+}
+
+type domainDNSListCountResponse struct {
+	Data int `json:"data"`
+}
+
+// DomainDNSListCount returns the number of domain DNS entries.
+func (a *Aftermarketpl) DomainDNSListCount(name string) (int, error) {
+	d := domainDNSListCountResponse{}
+	err := a.Do("/domain/dns/list/count", domainNameRequest{Name: name}, &d)
+	return d.Data, err
+}
+
+type domainDNSRemoveRequest struct {
+	EntryID int    `url:"entryId"`
+	Name    string `url:"name"`
+}
+
+type domainDNSRemoveResponse struct {
+	Data bool `json:"data"`
+}
+
+//DomainDNSRemove Remove DNS entry for domain.
+func (a *Aftermarketpl) DomainDNSRemove(name string, entryID int) (bool, error) {
+	request := domainDNSRemoveRequest{Name: name, EntryID: entryID}
+	d := domainDNSRemoveResponse{}
+	err := a.Do("/domain/dns/remove", request, &d)
+	return d.Data, err
+}
+
+const (
+	RecordTypeA     string = "A"
+	RecordTypeAAAA  string = "AAAA"
+	RecordTypeCNAME string = "CNAME"
+	RecordTypeMX    string = "MX"
+	RecordTypeTXT   string = "TXT"
+	RecordTypeNS    string = "NS"
+	RecordTypeCAA   string = "CAA"
+	RecordTypeSRV   string = "SRV"
+)
+
+type domainDNSAddRequest struct {
+	Name       string `url:"name"`
+	Value      string `url:"value"`
+	RecordType string `url:"type"`
+}
+
+type domainDNSAddResponse struct {
+	Data int `json:"data"`
+}
+
+//DomainDNSAdd adds new DNS entry for domain
+func (a *Aftermarketpl) DomainDNSAdd(name, value, recordType string) (int, error) {
+	request := domainDNSAddRequest{Name: name, Value: value, RecordType: recordType}
+	d := domainDNSAddResponse{}
+	err := a.Do("/domain/dns/add", request, &d)
 	return d.Data, err
 }
